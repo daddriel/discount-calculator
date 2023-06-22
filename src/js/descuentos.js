bf1d682d248115889dafee92d5bb8a516a40f1ed
David Adriazola <d.adriazolamuriel@gmail.com>
2023-06-21 22:37:52 -0400

    Validaciones

--- src/js/descuentos.js
+++ src/js/descuentos.js
@@ -1,35 +1,49 @@
 function discountPrice(price, off) {
-    return price * (100 - off)/100
+    return price * (100 - off) / 100
 }
 const listDiscount = {
-    '20WINTER' : 20,
-    '30SUMMER' : 30,
-    '50FRIDAY' : 50
+    '20WINTER': 20,
+    '30SUMMER': 30,
+    '50FRIDAY': 50
 }
-function onClickDiscount () {
+function onClickDiscount() {
     const inputPrice = document.getElementById('input-price').value;
     const inputDiscount = compareDiscount()
-    document.getElementById('price-final').innerText = 'The price with discount is $' + discountPrice( inputPrice, inputDiscount)
+
+    if (inputDiscount > 100) {
+        document.getElementById('price-final').innerText = 'The discount can not be more than 100%'
+    } else {
+        if (inputDiscount == 'error') {
+            document.getElementById('price-final').innerText = 'The discount code is not valid'
+        }
+        else {
+            document.getElementById('price-final').innerText = 'The price with discount is $' + discountPrice(inputPrice, inputDiscount)
+        }
+    }
+
 }
 
-function compareDiscount(){
+function compareDiscount() {
     const inputDiscount = document.getElementById('input-discount').value.toUpperCase();
     const typeDiscount = document.querySelector('input[name="typeDiscount"]:checked').value;
 
     var res = 0;
     if (typeDiscount == 'percent') {
         res = document.getElementById('input-discount').value;
-    }else{
+    } else {
         for (const [key, value] of Object.entries(listDiscount)) {
             if (inputDiscount == key) {
                 res = value;
+            }
+            else {
+                res = 'error';
             }
         }
     }
     return res;
 }
 
-function handleChange(src){
+function handleChange(src) {
     if (src.value == 'percent') {
         document.getElementById('input-discount').type = 'number';
         document.getElementById('discount-list').classList.add("hidden");
@@ -37,4 +51,10 @@ function handleChange(src){
         document.getElementById('input-discount').type = 'text';
         document.getElementById('discount-list').classList.remove("hidden");
     }
+}
+
+window.onload = function () {
+
+    handleChange(document.querySelector('input[name="typeDiscount"]:checked'));
+
 }
